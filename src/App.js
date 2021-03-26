956093f1b9a1e9ead9bcd3ef049274d05c7c8b50
temurbek-4224 <78374407+temurbek-4224@users.noreply.github.com>
2021-03-26 08:01:40 +0500

    updating our app with header component and link to homepage and shoppage. Thanks for Yihua and Adrei

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/hompage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='*'><div>404 Error</div></Route>
       </Switch>
     </div>
   );
